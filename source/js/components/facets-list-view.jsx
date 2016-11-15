d7fffef33775a541a9c3acfdb63c8b52be98da52
Darin Burris <darin.burris@amplificommerce.com>
2016-11-15 13:01:38 -0600

    - added updates to components in support of a11y eslint rules  -  cleaned up package file, removing no longer used dependencies  - removed no longer used grunt tasks

--- source/js/components/facets-list-view.jsx
+++ source/js/components/facets-list-view.jsx
@@ -5,7 +5,7 @@ export default class FacetsList extends React.Component{
 
 		return (
 
-			<li><a href="#">{this.props.categories}</a></li>
+			<li><label htmlFor={this.props.key}><input id={this.props.key} type="checkbox" role="checkbox" aria-checked="false" /><a href="http://www.google.com">{this.props.categories}</a></label></li>
 
 		);
 	}
