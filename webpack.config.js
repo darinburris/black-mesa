a01e74b2fc9f6f1102a25c75a96b045cc5aa7874
Darin Burris <darin.burris@amplificommerce.com>
2017-02-16 13:25:31 -0600

    - added cert and key file for running express locally over https  - updated express server to serve https  - updated v# of uglify package (package.json)  - updated wp config to support env parameters, both dev and prod and intorduced uglify plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,91 @@
+// function buildConfig(env) {
+// 	return require('./' + env + '.js')(
+// 		{
+// 			env: env
+// 		}
+// 	)
+// }
+//
+// module.exports = buildConfig;
+
+
+
 var path = require('path'),
 	webpack = require('webpack'),
 	entry = require('./amp-config').wpentry,
 	WebpackDevServer = require('webpack-dev-server');
 
-module.exports = {
-	entry: entry,
-	output: {
-		path: path.resolve(__dirname, './release/js'),
-		publicPath: '/js/',
-		filename: '[name].js'
-	},
-	devServer : {
-		inline: true,
-		port: 3333,
-		contentBase: './release',
-		publicPath: '/js/',
-		hot: true,
-		historyApiFallback: true,
-		stats: {
-			colors: true
-		}
-	},
-	module: {
-		rules: [//wp2
-//		loaders: [//wp1
-			{
-				test: /.jsx?$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-				query: {
-					presets: ['es2015','react','stage-2','react-hmre'],
-					plugins: ['transform-decorators-legacy']
+module.exports = function(env){
+
+	var env = env;
+
+	return {
+
+		entry: entry,
+		output: {
+			path: path.resolve(__dirname, './release/js'),
+			publicPath: '/js/',
+			filename: '[name].js'
+		},
+		devServer : {
+			inline: false,
+			port: 3333,
+			contentBase: './release',
+			publicPath: '/js/',
+			hot: false,
+			historyApiFallback: true,
+			stats: {
+				colors: true
+			}
+		},
+		module: {
+			rules: [//wp2
+	//		loaders: [//wp1
+				{
+					test: /.jsx?$/,
+					loader: 'babel-loader',
+					exclude: /node_modules/,
+					query: {
+						presets: ['es2015','react','stage-2','react-hmre'],
+						plugins: ['transform-decorators-legacy']
+					}
+				},
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader',
+					query: {
+						presets: ['es2015','react','stage-2','react-hmre'],
+						plugins: ['transform-decorators-legacy']
+					}
+				}		]
+		},
+		resolve: {
+			modules: [
+				path.join(__dirname, 'source'),
+				'node_modules',
+				'source/js/modules/',
+				'source/js/components/'
+			],
+			//modulesDirectories: [path.join(__dirname, 'source'), 'node_modules','source/js/modules/','source/js/components/'],
+			extensions: ['.js', '.jsx']
+		},
+		plugins: env !== 'develop' ? [
+			//new webpack.optimize.DedupePlugin(),
+			new webpack.optimize.OccurrenceOrderPlugin(),
+			new webpack.optimize.UglifyJsPlugin({ mangle: true, sourcemap: true }),
+			new webpack.DefinePlugin(
+				{
+					'process.env': {
+						'NODE_ENV': JSON.stringify('production')
+					}
 				}
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-				query: {
-					presets: ['es2015','react','stage-2','react-hmre'],
-					plugins: ['transform-decorators-legacy']
-				}
-			}		]
-	},
-
-
-
-	resolve: {
-		modules: [
-			path.join(__dirname, 'source'),
-			'node_modules',
-			'source/js/modules/',
-			'source/js/components/'
+			)
+		] : [
+			new webpack.HotModuleReplacementPlugin()
 		],
-		//modulesDirectories: [path.join(__dirname, 'source'), 'node_modules','source/js/modules/','source/js/components/'],
-		extensions: ['.js', '.jsx']
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin()
-	],
-	devtool: 'source-map'
+		devtool: 'cheap-module-source-map',
+
+	}
+
 };
